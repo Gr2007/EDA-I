26cc31043514f872a7781c3e0326bee9a5b15cb3
Gr2_srk <gvjohan99@gmail.com>
2017-03-05 15:47:50 -0600

    Create EJ02P04.c

---
+++ EJ02P04.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ( int argc, char **argv ){
+  printf("Ya has tu tarea Juan\n");
+  printf("lo subo el martes :v\n");
+  printf("este si hazlo tu alv :v");
+  getchar();
+  return 0;
+  }
