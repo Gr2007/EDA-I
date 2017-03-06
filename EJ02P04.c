cba0edcb23a945b1b9565aeedfe21f8f54eb11cc
Gr2_srk <gvjohan99@gmail.com>
2017-03-06 14:04:45 -0600

    minor fix
    *printf

--- EJ02P04.c
+++ EJ02P04.c
@@ -4,7 +4,7 @@ int main ( int argc, char **argv ){
   printf("Ya has tu tarea Juan\n");
   printf("lo subo el martes :v\n");
   printf("este si hazlo tu alv :v");
-  Printf("GRACIAS AMIGO :')");
+  printf("GRACIAS AMIGO :')");
   getchar();
   return 0;
   }
