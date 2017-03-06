1db85723f619f6e73fc20eb441f9e5eeb4ed4772
Junsk <homito_randy@hotmail.com>
2017-03-06 13:52:12 -0600

    Update EJ02P04.c

--- EJ02P04.c
+++ EJ02P04.c
@@ -4,6 +4,7 @@ int main ( int argc, char **argv ){
   printf("Ya has tu tarea Juan\n");
   printf("lo subo el martes :v\n");
   printf("este si hazlo tu alv :v");
+  Printf("GRACIAS AMIGO :')");
   getchar();
   return 0;
   }
